6b9c3a0ea55de6daa683e6015b3311471d91e58f
patranakamo <patranakamo@gmail.com>
2024-03-05 14:56:50 +0700

    update config page to save auth data too.

--- src/stores/config.js
+++ src/stores/config.js
@@ -7,13 +7,12 @@ axios.defaults.baseURL = '/v1/';
 export const useConfigStore = defineStore('config',
     {
         state: () => ({
-            listWebHook: {
-                "web_hook_url_newaction": "",
-                "web_hook_url_start": "",
-                "web_hook_url_end": "",
-                "web_hook_url_update_invoice": ""
-            },
+            webHookUrl: "",
+            header_key: "",
+            api_key: "",
+            internal_note: "",
             selectedOptionOpen: "open",
+            authOption: "close",
             selectedOption: 4,
             inputValues: [10, 20, 30, 40, 50, 60],
         }),
@@ -23,39 +22,56 @@ export const useConfigStore = defineStore('config',
             }
         },
         actions: {
-            async loadConfig(router) {
+            async loadConfig() {
                 try {
                     const returnData = await axios.get(`/config/webhook`)
                     if (!returnData.data.data.config_data) {
                         toastr.info('no data return after load');
                     }
                     const configData = returnData.data.data.config_data
-                    this.listWebHook = configData
 
                     this.selectedOptionOpen = configData.status
-                    this.selectedOption = configData.numRetry
-                    this.inputValues = JSON.parse(configData.arrayRetry);
-                } catch (error) {
-                    if (error.response.statusText === 'Unauthorized') {
-                        router.push({name: 'SignInPage'})
+                    if (configData.auth_option) {
+                        this.authOption = configData.auth_option
+                        this.bearer = configData.bearer
+                        this.header_key = configData.header_key
+                        this.api_key = configData.customized_api_key
                     }
-                    console.log("error case get data ",)
+                    this.selectedOption = configData.numRetry
+                    this.webHookUrl = configData.web_hook_url_update_invoice
+                    this.internal_note = configData.internal_note
+
+                    if (configData.arrayRetry) {
+                        this.inputValues = JSON.parse(configData.arrayRetry);
+                    }
+                } catch (error) {
+                    console.log("error case get data ", error)
                 }
             },
-            async updateConfig(editConfig) {
+            async updateConfig() {
                 try {
-                    const updateData = {
-                        // web_hook_url_newaction: editConfig.newaction,
-                        // web_hook_url_start: editConfig.start,
-                        // web_hook_url_end: editConfig.end
-                        web_hook_url_update_invoice: editConfig.update_invoice
+                    let updateData = {
+                        web_hook_url_update_invoice: this.webHookUrl,
+                        auth_option: this.authOption,
+                        internal_note: this.internal_note
+                    }
+                    if (this.authOption === 'bearer') {
+                        updateData.bearer = this.bearer
+                    }
+                    if (this.authOption === 'customized') {
+                        updateData.header_key = this.header_key
+                        updateData.customized_api_key = this.api_key
                     }
                     const returnData = await axios.post(`/config/webhook/save`, updateData)
                     console.log(returnData.data)
                     toastr.success('update webhook url success');
                 } catch (error) {
-                    toastr.error('update webhook url fail');
-                    console.log(error)
+                    if (error.response.data.data.message) {
+                        const messageError = error.response.data.data.message
+                        toastr.warning('update webhook url fail ' + messageError);
+                    } else {
+                        toastr.error('update webhook url fail ');
+                    }
                 }
             },
             async updateConfigRetryTime() {
